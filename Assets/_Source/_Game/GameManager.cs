2c733f0c8a4d7f4553a420306017c8f247a74adb
Erentsen Sangadzhiev <edsangadzhiev@edu.hse.ru>
2023-11-14 13:40:09 +0300

    fix(code): fixed logic in GameManager

--- Assets/_Source/_Game/GameManager.cs
+++ Assets/_Source/_Game/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using _Source._Core;
 using UnityEngine;
 
@@ -5,11 +6,27 @@ namespace _Source._Game
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] 
+        private List<GameObject> _list = new();
         private ResourceBank _resourceBank;
         
         public void Awake()
         {
             _resourceBank = new ResourceBank();
+            
+            _resourceBank.ChangeResource(GameResource.Humans, 0);
+            _resourceBank.ChangeResource(GameResource.Food, 0);
+            _resourceBank.ChangeResource(GameResource.Wood, 0);
+            _resourceBank.ChangeResource(GameResource.Stone, 0);
+            _resourceBank.ChangeResource(GameResource.Gold, 0);
+
+            foreach (var gameObj in _list)
+            {
+                if (gameObj.TryGetComponent(out ResourceVisual resourceVisual))
+                {
+                    resourceVisual.Init(_resourceBank);
+                }
+            }
             
             _resourceBank.ChangeResource(GameResource.Humans, 10);
             _resourceBank.ChangeResource(GameResource.Food, 5);
