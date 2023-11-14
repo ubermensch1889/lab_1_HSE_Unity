ff961d9f123ae62a2f62e773bce2f7ee1395b405
Erentsen Sangadzhiev <edsangadzhiev@edu.hse.ru>
2023-11-14 13:41:08 +0300

    fix(code): fixed logic in ChangeResource method of ResourceBank

--- Assets/_Source/_Core/ResourceBank.cs
+++ Assets/_Source/_Core/ResourceBank.cs
@@ -6,11 +6,18 @@ namespace _Source._Core
 {
     public class ResourceBank
     {
-        private Dictionary<GameResource, ObservableInt> _resourceDict;
+        private Dictionary<GameResource, ObservableInt> _resourceDict = new();
 
         public void ChangeResource(GameResource r, int v)
         {
-            _resourceDict[r] = new ObservableInt(_resourceDict[r].Value + v);
+            if (_resourceDict.ContainsKey(r))
+            {
+                _resourceDict[r].Value += v;
+            }
+            else
+            {
+                _resourceDict.Add(r, new ObservableInt(v));
+            }
         }
 
         public ObservableInt GetResource(GameResource r)
